df2530890a31bc779aefd17c87e96b399c7d5363
abdoulaye wane <abdoulaye.wane@saint-benigne.fr>
2023-11-09 17:02:45 +0100

    Modif GetCsv for Bdd

--- AdministrationLaravel/app/Http/Controllers/QuizController.php
+++ AdministrationLaravel/app/Http/Controllers/QuizController.php
@@ -5,11 +5,50 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use League\Csv\Reader;
+use App\Models\Quiz; 
+
 
 class QuizController extends Controller
 {
-    //
-
+   
+    // public function GetCsv(Request $request)
+    // {
+    //     if ($request->hasFile('csv_file')) {
+    //         $file = $request->file('csv_file');
+    
+    //         $path = $file->storeAs('csv_file', $file->getClientOriginalName());
+    
+    //         $filenameWithoutExtension = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+    
+    //         $file = fopen(storage_path('app/' . $path), "r");
+    
+    //         if ($file) {
+    //             fgetcsv($file, 0, ';'); // Sautez la première ligne (ligne d'en-tête)
+    
+    //             while (($row = fgetcsv($file, 0, ';')) !== false) {
+    //                 $row = array_map('trim', $row);
+    
+    //                 Quiz::create([
+    //                     'nom_du_quiz' => $filenameWithoutExtension,
+    //                     'question' => mb_convert_encoding($row[0], 'UTF-8', 'auto'),
+    //                     'bonnes_reponses' => mb_convert_encoding($row[1], 'UTF-8', 'auto'),
+    //                     'mauvaises_reponses' => mb_convert_encoding($row[2], 'UTF-8', 'auto'),
+    //                     'tags' => mb_convert_encoding($row[3], 'UTF-8', 'auto'),
+    //                 ]);
+    //             }
+    
+    //             fclose($file);
+    //             Storage::delete($path);
+    
+    //             // Redirigez l'utilisateur vers une autre page après traitement
+    //             return view('dashboard/quizConfirmation', compact('filenameWithoutExtension'));
+    //         } else {
+    //             return "Impossible d'ouvrir le fichier.";
+    //         }
+    //     }
+    
+    //     return 'Aucun fichier téléchargé.';
+    // }
     public function GetCsv(Request $request)
 {
     // Vérifiez si un fichier a été téléchargé
@@ -59,50 +98,5 @@ class QuizController extends Controller
 
     return 'Aucun fichier téléchargé.';
 }
-
-
-    
-
-//     public function GetCsv(Request $request)
-// {
-//     // Vérifiez si un fichier a été téléchargé
-//     if ($request->hasFile('csv_file')) {
-//         $file = $request->file('csv_file');
-
-//         $path = $file->storeAs('csv_file', $file->getClientOriginalName());
-
-//         $file = fopen(storage_path('app/' . $path), "r");
-
-//         if ($file) {
-//             $dataCsv = array();
-
-//             while (($row = fgetcsv($file)) !== false) {
-//                 $entry = array(
-//                     'question' => $row[0],
-//                     'bonnes_reponses' => isset($row[1]) ? explode(';', $row[1]) : [],
-//                     'mauvaises_reponses' => isset($row[2]) ? explode(';', $row[2]) : [],
-//                     'tags' => isset($row[3]) ? explode(';', $row[3]) : []
-//                 );
-
-//                 $dataCsv[] = $entry;
-//             }
-
-//             fclose($file);
-
-//             Storage::delete($path);
-
-//             // Redirigez l'utilisateur vers une autre page après traitement
-//             // Avec le tableau associatif avec les données
-//             return view('dashboard/quizConfirmation', compact('dataCsv'));
-//         } else {
-//             echo "Impossible d'ouvrir le fichier.";
-//         }
-
-//         return 'Fichier téléchargé avec succès!';
-//     }
-
-//     return 'Aucun fichier téléchargé.';
-// }
-
 
 }
--- AdministrationLaravel/app/Models/Quiz.php
+++ AdministrationLaravel/app/Models/Quiz.php
@@ -5,7 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// class Quiz extends Model
+// {
+//     use HasFactory;
+// }
 class Quiz extends Model
 {
-    use HasFactory;
+    protected $fillable = ['nom_du_quiz', 'question', 'bonnes_reponses', 'mauvaises_reponses', 'tags'];
 }
